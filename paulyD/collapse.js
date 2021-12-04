cbaddfacb613d8dc4bdb94e5a34c96c02c3fb3fa
jlinton <3174195@student.ocadu.ca>
2021-12-03 21:35:08 -0700

    Paul Debevec - a2.1
    Converting Paul's inline CSS to external + reforming layout with a grid instead of tables.

---
+++ paulyD/collapse.js
@@ -0,0 +1,13 @@
+// document.querySelectorAll('.accordionButton').forEach(button => {
+//   button.addEventListener('click', () => {
+//     const accordionContent = button.nextElementSibling;
+
+//     button.classList.toggle('accordionButton--active');
+
+//     if(button.classList.contains('accordionButton--active')) {
+//       accordionContent.style.maxHeight = 100%;
+//     } else {
+//             accordionContent.style.maxHeight = 0;
+//     }
+//   });
+// });
